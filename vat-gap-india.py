c4d5a702e30b98fdfb6ad395007696c1403dfdbc
kumar-ab <iitkgp.abhinav@gmail.com>
2019-03-16 00:34:26 +0530

    Tax gap updates

--- vat-gap-india.py
+++ vat-gap-india.py
@@ -37,58 +37,45 @@ def import_Excel_SUT_2014(year):
     tax_subsidies_col = col2num(tax_subsidies_col_excel)
 
     '''
-    For Latvia EU columns and non-EU columns
-    '''
-    Import_col_eu_excel = "BQ"
-    Import_col_noneu_excel = "BT"
-    Import_col_eu = col2num(Import_col_eu_excel)
-    Import_col_noneu = col2num(Import_col_noneu_excel)
-
-    '''
     USE table
     '''
     use_start_col_excel="C"
-    use_end_col_excel="BO"
+    use_end_col_excel="BP"
     use_start_col=col2num(use_start_col_excel)
     use_end_col=col2num(use_end_col_excel)
-    use_start_row=6
-    use_end_row=70
+    use_start_row=4
+    use_end_row=143
 
-    use_col_product_id_excel = "A"
+    use_col_product_id_excel = "B"
     use_col_product_id = col2num(use_col_product_id_excel)
-    use_row_sector_id = 5
+    use_row_sector_id = 2
 
-    fin_cons_hh_col_excel = "BQ"
-    fin_cons_np_col_excel = "BR"
+    fin_cons_hh_col_excel = "BR"
+    # fin_cons_np_col_excel = "BR"
     fin_cons_gov_col_excel = "BS"
 
     fin_cons_hh_col = col2num(fin_cons_hh_col_excel)
-    fin_cons_np_col = col2num(fin_cons_np_col_excel)
+    # fin_cons_np_col = col2num(fin_cons_np_col_excel)
     fin_cons_gov_col = col2num(fin_cons_gov_col_excel)
 
-    gcf_col_excel ="BY"
+    gcf_col_excel ="BT"
     gcf_col = col2num(gcf_col_excel)
 
-    Export_col_excel = "CD"
+    Export_col_excel = "BW"
     Export_col = col2num(Export_col_excel)
 
-    '''
-    For Latvia EU columns and non-EU columns
-    '''
-    Export_col_eu_excel = "BZ"
-    Export_col_noneu_excel = "CC"
-    Export_col_eu = col2num(Export_col_eu_excel)
-    Export_col_noneu = col2num(Export_col_noneu_excel)
-
-    df = pd.read_excel('India Supply Use Table SUT_12-13_dated_280916_working_copy.xlsx', sheet_name='supply 2012-13')
-    #df1 = df.iloc[supply_start_row-2:supply_end_row-1,supply_start_col-1:supply_end_col-1]
-    #df2 = df1.fillna(0)
-    #
+    df = pd.read_excel('India Supply Use Table SUT_12-13.xlsx', sheet_name='Supply 2012-13', header=None, skiprows=1)
+    df_rates = pd.read_excel('India Supply Use Table SUT_12-13.xlsx', sheet_name='Rates')
 
     df1 = df.iloc[:supply_end_row-1,:supply_end_col]
+    df1 = df1.fillna(0)
     df1.columns = df1.iloc[supply_row_sector_id-2,:]
-    df1.index = df1.iloc[:,supply_col_product_id-1]
+    df1.index = df1.iloc[:,supply_col_product_id-2]
     df2 = df1.iloc[supply_start_row-2:,supply_start_col-1:]
+    supply_mat = df1.values[2:,2:]
+    rates = df_rates.values[:,1]
+    rate_vec = rates.reshape(rates.shape[0], 1)
+    tax_supply = supply_mat * rate_vec
     supply_plusdf = df2.fillna(0)
     supply_plus_transdf = supply_plusdf.transpose()
 
@@ -101,14 +88,6 @@ def import_Excel_SUT_2014(year):
     df2 = df1.fillna(0)
     imports = df2.values
 
-    df1 = df.iloc[supply_start_row-2:supply_end_row-1, Import_col_eu-1]
-    df2 = df1.fillna(0)
-    imports_eu = df2.values
-
-    df1 = df.iloc[supply_start_row-2:supply_end_row-1, Import_col_noneu-1]
-    df2 = df1.fillna(0)
-    imports_noneu = df2.values
-
     df1 = df.iloc[supply_start_row-2:supply_end_row-1, trade_margin_col-1]
     df2 = df1.fillna(0)
     trade_margins = df2.values
@@ -119,15 +98,15 @@ def import_Excel_SUT_2014(year):
     tax_subsidies = df2.values
     tax_subsidiesdf = pd.DataFrame(data=tax_subsidies, index=product_headers, columns=np.array(['Tax and Subsidies']))
 
-    df = pd.read_excel('Supply Use tables - 2014.xlsx', sheet_name='USE 2014')
-#    df1 = df.iloc[use_start_row-2:use_end_row-1,use_start_col-1:use_end_col-1]
-#    df2 = df1.fillna(0)
-#    use = df2.values
+    df_use = pd.read_excel('India Supply Use Table SUT_12-13.xlsx', sheet_name='Use 2012-13', header=None, skiprows=1)
 
-    df1 = df.iloc[:use_end_row-1,:use_end_col]
-    df1.columns  =df1.iloc[use_row_sector_id-2,:]
-    df1.index = df1.iloc[:,use_col_product_id-1]
+    df1 = df_use.iloc[:use_end_row-1,:use_end_col]
+    df1 = df1.fillna(0)
+    df1.columns = df1.iloc[use_row_sector_id-2,:]
+    df1.index = df1.iloc[:,use_col_product_id-2]
     df2 = df1.iloc[use_start_row-2:,use_start_col-1:]
+    use_mat = df1.values[2:,2:]
+    tax_use = use_mat * rate_vec
     use_plusdf = df2.fillna(0)
 
     df1 = df.iloc[use_start_row-2:use_end_row-1, fin_cons_hh_col-1]
@@ -150,14 +129,6 @@ def import_Excel_SUT_2014(year):
     df2 = df1.fillna(0)
     exports = df2.values
 
-    df1 = df.iloc[use_start_row-2:use_end_row-1, Export_col_eu-1]
-    df2 = df1.fillna(0)
-    exports_eu = df2.values
-
-    df1 = df.iloc[supply_start_row-2:supply_end_row-1, Export_col_noneu-1]
-    df2 = df1.fillna(0)
-    exports_noneu = df2.values
-
 #    tot_sup_comm=supply.sum(axis=1)
 #    tot_use_comm=use.sum(axis=1)
 
@@ -165,161 +136,6 @@ def import_Excel_SUT_2014(year):
 
     return (supply_plusdf, supply_plus_transdf, use_plusdf, sector_headers, product_headers, imports_eu, imports_noneu, trade_marginsdf, tax_subsidiesdf, exports_eu, exports_noneu, fin_cons, gcf)
 
-def import_Excel_SUT_2013(year):
-
-    # First prepare the Excel file by Selecting the entire sheet and unmerging any merged cells
-    '''
-    SUPPLY table
-    '''
-    supply_start_col_excel="E"
-    supply_end_col_excel = "BQ"
-    supply_start_col = col2num(supply_start_col_excel)
-    supply_end_col=col2num(supply_end_col_excel)
-    supply_start_row=8
-    supply_end_row=72
-
-    supply_col_product_id_excel = "C"
-    supply_col_product_id = col2num(supply_col_product_id_excel)
-    supply_row_sector_id = 5
-
-    Import_col_excel = "BW"
-    Import_col = col2num(Import_col_excel)
-
-    trade_margin_col_excel = "BY"
-    trade_margin_col = col2num(trade_margin_col_excel)
-
-    tax_subsidies_col_excel = "BZ"
-    tax_subsidies_col = col2num(tax_subsidies_col_excel)
-
-    '''
-    For Latvia EU columns and non-EU columns
-    '''
-    Import_col_eu_excel = "BS"
-    Import_col_noneu_excel = "BV"
-    Import_col_eu = col2num(Import_col_eu_excel)
-    Import_col_noneu = col2num(Import_col_noneu_excel)
-
-    '''
-    USE table
-    '''
-    use_start_col_excel="E"
-    use_end_col_excel="BQ"
-    use_start_col=col2num(use_start_col_excel)
-    use_end_col=col2num(use_end_col_excel)
-    use_start_row=8
-    use_end_row=72
-
-    use_col_product_id_excel = "C"
-    use_col_product_id = col2num(use_col_product_id_excel)
-    use_row_sector_id = 5
-
-    fin_cons_hh_col_excel = "BS"
-    fin_cons_np_col_excel = "BT"
-    fin_cons_gov_col_excel = "BU"
-
-    fin_cons_hh_col = col2num(fin_cons_hh_col_excel)
-    fin_cons_np_col = col2num(fin_cons_np_col_excel)
-    fin_cons_gov_col = col2num(fin_cons_gov_col_excel)
-
-    gcf_col_excel ="CA"
-    gcf_col = col2num(gcf_col_excel)
-
-    Export_col_excel = "CF"
-    Export_col = col2num(Export_col_excel)
-
-    '''
-    For Latvia EU columns and non-EU columns
-    '''
-    Export_col_eu_excel = "CB"
-    Export_col_noneu_excel = "CE"
-    Export_col_eu = col2num(Export_col_eu_excel)
-    Export_col_noneu = col2num(Export_col_noneu_excel)
-
-
-    df = pd.read_excel('Supply Use Tables - 2011-2013.xlsx', sheet_name='Supply_2013')
-    #df1 = df.iloc[supply_start_row-2:supply_end_row-1,supply_start_col-1:supply_end_col-1]
-    #df2 = df1.fillna(0)
-    #
-
-    df1 = df.iloc[:supply_end_row-1,:supply_end_col]
-    df1.columns = df1.iloc[supply_row_sector_id-2,:]
-    df1.index = df1.iloc[:,supply_col_product_id-1]
-    df2 = df1.iloc[supply_start_row-2:,supply_start_col-1:]
-    supply_plusdf = df2.fillna(0)
-    supply_plus_transdf = supply_plusdf.transpose()
-
-    sector_headers = df1.iloc[supply_row_sector_id-2,:]
-    product_headers = df1.iloc[:,supply_col_product_id-1]
-    sector_headers = sector_headers[4:].values
-    product_headers = product_headers[6:].values
-
-    df1 = df.iloc[supply_start_row-2:supply_end_row-1, Import_col-1]
-    df2 = df1.fillna(0)
-    imports = df2.values
-
-    df1 = df.iloc[supply_start_row-2:supply_end_row-1, Import_col_eu-1]
-    df2 = df1.fillna(0)
-    imports_eu = df2.values
-
-    df1 = df.iloc[supply_start_row-2:supply_end_row-1, Import_col_noneu-1]
-    df2 = df1.fillna(0)
-    imports_noneu = df2.values
-
-    df1 = df.iloc[supply_start_row-2:supply_end_row-1, trade_margin_col-1]
-    df2 = df1.fillna(0)
-    trade_margins = df2.values
-    trade_marginsdf = pd.DataFrame(data=trade_margins, index=product_headers, columns=np.array(['Trade Margins']))
-
-    df1 = df.iloc[supply_start_row-2:supply_end_row-1, tax_subsidies_col-1]
-    df2 = df1.fillna(0)
-    tax_subsidies = df2.values
-    tax_subsidiesdf = pd.DataFrame(data=tax_subsidies, index=product_headers, columns=np.array(['Tax and Subsidies']))
-
-    df = pd.read_excel('Supply Use Tables - 2011-2013.xlsx', sheet_name='Use_2013')
-#    df1 = df.iloc[use_start_row-2:use_end_row-1,use_start_col-1:use_end_col-1]
-#    df2 = df1.fillna(0)
-#    use = df2.values
-
-    df1 = df.iloc[:use_end_row-1,:use_end_col]
-    df1.columns  =df1.iloc[use_row_sector_id-2,:]
-    df1.index = df1.iloc[:,use_col_product_id-1]
-    df2 = df1.iloc[use_start_row-2:,use_start_col-1:]
-    use_plusdf = df2.fillna(0)
-
-    df1 = df.iloc[use_start_row-2:use_end_row-1, fin_cons_hh_col-1]
-    df2 = df1.fillna(0)
-    fin_cons_hh = df2.values
-
-    df1 = df.iloc[use_start_row-2:use_end_row-1,fin_cons_np_col-1]
-    df2 = df1.fillna(0)
-    fin_cons_np = df2.values
-
-    df1 = df.iloc[use_start_row-2:use_end_row-1, fin_cons_gov_col-1]
-    df2 = df1.fillna(0)
-    fin_cons_gov = df2.values
-
-    df1 = df.iloc[use_start_row-2:use_end_row-1, gcf_col-1]
-    df2 = df1.fillna(0)
-    gcf = df2.values
-
-    df1 = df.iloc[use_start_row-2:use_end_row-1, Export_col-1]
-    df2 = df1.fillna(0)
-    exports = df2.values
-
-    df1 = df.iloc[use_start_row-2:use_end_row-1, Export_col_eu-1]
-    df2 = df1.fillna(0)
-    exports_eu = df2.values
-
-    df1 = df.iloc[supply_start_row-2:supply_end_row-1, Export_col_noneu-1]
-    df2 = df1.fillna(0)
-    exports_noneu = df2.values
-
-#    tot_sup_comm=supply.sum(axis=1)
-#    tot_use_comm=use.sum(axis=1)
-
-    fin_cons=fin_cons_hh+fin_cons_np+fin_cons_gov
-
-    return (supply_plusdf, supply_plus_transdf, use_plusdf, sector_headers, product_headers, imports_eu, imports_noneu, trade_marginsdf, tax_subsidiesdf, exports_eu, exports_noneu, fin_cons, gcf)
 
 def import_tax_rates():
     # Import the tax rates
